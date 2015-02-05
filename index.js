fe1eebb72567b42e9d5d54c048f58cd244246b49
Paweł Sroka <srokap@gmail.com>
2015-02-05 05:00:33 +0100

    Added hmc5883l compass support and utils.

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
 "use strict";
 
-var pressure = require('./lib/bmp085')(0x77)
+var pressure = require('./lib/devices/bmp085')(0x77)
+var compass = require('./lib/devices/hmc5883l')(0x1E)
+var utils = require('./lib/utils')
 
-pressure.init(function(){
+//setInterval(function(){
+//  utils.scanDevices(1, function(err, res) {
+//    console.log('result data', err, res)
+//  })
+//}, 500)
+
+//pressure.init(function(){
+//  setInterval(function(){
+//    pressure.getPressure(function(err, p, T){
+//      if (err) {
+//        console.error(err)
+//      } else {
+//        console.log('Temperature', T, p)
+//      }
+//    })
+//  }, 500)
+//})
+
+
+
+compass.init(function(){
   setInterval(function(){
-    pressure.getPressure(function(err, p, T){
+    compass.getReading(function(err, X, Y, Z){
       if (err) {
         console.error(err)
       } else {
-        console.log('Temperature', T, p)
+        console.log('Compass', X, Y, Z)
       }
     })
   }, 500)
